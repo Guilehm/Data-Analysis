83f1b90ab58ca8617d87609c78ffa017edd86990
guile.hm <guile.hm@hotmail.com>
2018-05-26 13:38:47 -0300

    Create colormap

--- scatter_squares.py
+++ scatter_squares.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, s=40, edgecolors='none', c=y_values, cmap=plt.cm.Blues)
 
 # define the graph title and rename axis
 plt.title('Square Numbers', fontsize=24)
