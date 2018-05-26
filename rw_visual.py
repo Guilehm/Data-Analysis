08203a2bf77974e40f433e41465ea90534fbdea5
guile.hm <guile.hm@hotmail.com>
2018-05-26 14:03:48 -0300

    Create loop

--- rw_visual.py
+++ rw_visual.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 # create random walk and plot
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input('Make another walk? y/n: ').lower()
+    if keep_running == 'n':
+        break
