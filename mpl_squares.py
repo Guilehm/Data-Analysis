9eee877b5d37b5099d41c7a4fc0fe476dde3bbf6
Guilherme Luis <guile.hm@hotmail.com>
2018-05-26 12:09:33 -0300

    Insert settings

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,5 +2,16 @@ import matplotlib.pyplot as plt
 
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# show title and axis names
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('Value', fontsize=14)
+plt.ylabel('Square of Value', fontsize=14)
+
+# define the size of labels
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
+
+
