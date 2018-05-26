648042a08d8cabb1ad9126f51cb87a618e32c139
guile.hm <guile.hm@hotmail.com>
2018-05-26 14:06:29 -0300

    Colorfy

--- rw_visual.py
+++ rw_visual.py
@@ -6,7 +6,10 @@ from random_walk import RandomWalk
 while True:
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Reds,
+                edgecolors='none', s=15)
     plt.show()
 
     keep_running = input('Make another walk? y/n: ').lower()
