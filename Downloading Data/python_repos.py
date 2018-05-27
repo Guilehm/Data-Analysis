b74a901e786d9782f682365743f342019ab39173
guile.hm <guile.hm@hotmail.com>
2018-05-27 19:09:16 -0300

    Returning most starred repos

--- Downloading Data/python_repos.py
+++ Downloading Data/python_repos.py
@@ -12,6 +12,14 @@ print("Total repositories:", response_dict['total_count'])
 # Explore information about the repositories
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print("\nSelected information about first repository:")
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])
 
 #  Examine the first repository
 repo_dict = repo_dicts[0]
